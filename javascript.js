da3d37d38c974c2d52ac9b49b012a448bd47e1b5
Underthesea2009 <131215067+Underthesea2009@users.noreply.github.com>
2023-07-19 19:02:04 -0400

    Update javascript.js
    added a welcome to Ariel Mccully's website

---
+++ javascript.js
@@ -0,0 +1,6 @@
+<script>
+        // Simple JavaScript code to display an alert when the page is loaded
+        window.onload = function() {
+            alert('Welcome to Ariel McCully\'s website!')
+        }
+    </script>
